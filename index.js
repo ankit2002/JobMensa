f36225f1ee2ed2eeaa4895756a3c77b82b1ddc0f
Ankit Mishra <ANKIT@megas-MacBook-Pro.local>
2016-01-12 16:01:17 +0100

    makes body parser

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 var express = require('express');
 var http = require('http');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port',process.env.PORT || 8000);
+app.set('views', './views')
+app.set('view engine','jade');
+
+app.use('/', express.static(__dirname + '/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get ('/', function (req,res) {
+    res.render('login');
+});
+
+require('./db.js')(app);
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+    console.log('Server listening on port ' + app.get('port'));
+});
