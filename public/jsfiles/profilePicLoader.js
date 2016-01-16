317aadc5746df58e152fda94270f9512137a0d3c
Ankit Mishra <ANKIT@megas-MacBook-Pro.local>
2016-01-16 18:29:47 +0100

    bug with structure

---
+++ public/jsfiles/profilePicLoader.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ANKIT on 14/12/15.
+ */
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#profile_pic')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(150);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
